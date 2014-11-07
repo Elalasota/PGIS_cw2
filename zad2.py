94deb63158ee10c863c5148d254bc9d687405cbe
Elalasota <elcialas@gmail.com>
2014-11-07 17:36:19 +0100

    Cw 4
    Dodano cw 4+drobne poprawki

--- zad2.py
+++ zad2.py
@@ -1,15 +1,24 @@
-plik=open("plik.txt", "r")
+plik=open("menu.txt", "r")
+lista=[]
 tresc=plik.read()
-Menu={"Rosol":3.50, "Placki":6.80, "Schabowy":12.20, "Pomidorowa":3.50, "Pizza":19.20, "Surowka":3.50}
+tresc=tresc.replace("\n", " ")
+lista=tresc.split(" ")
+klucze1=[]
+wartosci1=[]
+for i in xrange(0, len(lista), 2):
+    klucze1.append(lista[i])
+    wartosci1.append(float(lista[i+1]))
+Menu=dict(zip(klucze1, wartosci1))
 plik.close()
+
 def platnosc(zamowienie):
     wartosc=0
     for pozycja in zamowienie:
         if pozycja in Menu.keys():
             wartosc=wartosc+Menu[pozycja]
-    napiwek=wartosc*0.1
+    napiwek=round(wartosc*0.1, 2)
     print napiwek+wartosc
     return napiwek+wartosc
 
-dania=["Rosol", "Pomidorowa", "Pizza", "Surowka"]
+dania=["Rosol", "Surowka", "Schabowy", "Ogorek"]
 platnosc(dania)
